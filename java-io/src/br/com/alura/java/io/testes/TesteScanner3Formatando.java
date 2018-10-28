dca3b4c6d07e65c0b702b73f3d67809e7cb37b8a
Leonardo de Souza Faria <leonardo.fari@hotmail.com>
2018-10-28 00:21:57 -0300

    Utilizando o format para formatar string

---
+++ java-io/src/br/com/alura/java/io/testes/TesteScanner3Formatando.java
@@ -0,0 +1,46 @@
+package br.com.alura.java.io.testes;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteScanner3Formatando {
+
+	public static void main(String[] args) {
+
+		try {
+
+			Scanner scanner = new Scanner(new File("contas.csv"));
+
+			while (scanner.hasNextLine()) {
+				
+				String linha = scanner.nextLine();
+				
+				Scanner linhaScanner = new Scanner(linha);
+				linhaScanner.useLocale(Locale.US);
+				linhaScanner.useDelimiter(",");
+				
+				
+				
+				String valor1 = linhaScanner.next();
+				int valor2 = linhaScanner.nextInt();
+				int valor3 = linhaScanner.nextInt();
+				String valor4 = linhaScanner.next();
+				Double valor5 = linhaScanner.nextDouble();
+				
+				System.out.format(new Locale("pt", "BR")," %s %04d %08d %s  %08.2f  %n" ,valor1 , valor2 , valor3 , valor4 , valor5);
+
+				linhaScanner.close();
+			}
+			
+			scanner.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+			System.out.println(e.getMessage());
+
+		}
+
+	}
+
+}
